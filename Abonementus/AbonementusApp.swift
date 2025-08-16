61bda39d26ef5e347cd17355b40a941bacef51b5
Mike Tevelev <miketevelev@gmail.com>
2025-08-16 20:05:52 +0300

    Initial Commit

---
+++ Abonementus/AbonementusApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbonementusApp.swift
+//  Abonementus
+//
+//  Created by Mike Tevelev on 16.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct AbonementusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AbonementusTests/AbonementusTests.swift
@@ -0,0 +1,17 @@
+//
+//  AbonementusTests.swift
+//  AbonementusTests
+//
+//  Created by Mike Tevelev on 16.08.2025.
+//
+
+import Testing
+@testable import Abonementus
+
+struct AbonementusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
